2b8d1c0b6e4bf0ca28c4111e7c0395151638739e
Alex Jenkins <alexanderjenkins96@outlook.com>
2020-01-23 17:54:21 +0000

    Introduction to packages complete.

---
+++ week9-week9_17.FirstPackages/src/mooc/ui/TextUserInterface.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Alex Jenkins
+ */
+package mooc.ui;
+
+public class TextUserInterface implements UserInterface {
+    
+    
+    public void update() {
+        System.out.println("Updating the user interface");
+    }
+    
+}
---
+++ week9-week9_17.FirstPackages/src/mooc/ui/UserInterface.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author Alex Jenkins
+ */
+
+package mooc.ui;
+
+public interface UserInterface {
+    
+    void update();
+}
