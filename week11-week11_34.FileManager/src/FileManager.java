be6f1970216d372abc3437c6026243f875d6e8d1
Alex Jenkins <alexanderjenkins96@outlook.com>
2020-02-10 11:00:02 +0000

    Reading and writing files. Complete.

--- week11-week11_34.FileManager/src/FileManager.java
+++ week11-week11_34.FileManager/src/FileManager.java
@@ -10,12 +10,29 @@ import java.util.Scanner;
 public class FileManager {
 
     public List<String> read(String file) throws FileNotFoundException {
-        return null;
+        // Read from the file directory passed as parameter
+        File f = new File(file);
+        Scanner reader = new Scanner(f);
+        ArrayList<String> fContents = new ArrayList<String>();
+        while(reader.hasNextLine()) {
+            fContents.add(reader.nextLine());
+        }
+        return fContents;
     }
 
     public void save(String file, String text) throws IOException {
+        // Write the parameter text to file. If file exists, overwrite
+        FileWriter writer = new FileWriter(file);
+        writer.write(text);
+        writer.close();
     }
 
     public void save(String file, List<String> texts) throws IOException {
+        // Write the list of strings to file. If file exists, overwrite
+        FileWriter writer = new FileWriter(file);
+        for (String line : texts) {
+            writer.write(line + "\n");
+        }
+        writer.close();
     }
 }
--- week11-week11_34.FileManager/src/Main.java
+++ week11-week11_34.FileManager/src/Main.java
@@ -1,10 +1,15 @@
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         FileManager t = new FileManager();
-
+        ArrayList<String> texts = new ArrayList<String>();
+        texts.add("toot");
+        texts.add("soot");
+        texts.add("boot");
+        t.save("src/testinput1.txt", texts);
         for (String line : t.read("src/testinput1.txt")) {
             System.out.println(line);
         }
