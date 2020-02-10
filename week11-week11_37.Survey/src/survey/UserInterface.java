b30af007fe4cfe309df1b7a28f9d0a916ab5b3a2
Alex Jenkins <alexanderjenkins96@outlook.com>
2020-02-10 12:49:02 +0000

    Creater a GUI for a suvery. Features JLabel, JRadioButton, JCheckBox, JButton and ButtonGroup. Complete.

--- week11-week11_37.Survey/src/survey/UserInterface.java
+++ week11-week11_37.Survey/src/survey/UserInterface.java
@@ -11,6 +11,32 @@ public class UserInterface implements Runnable {
     @Override
     public void run() {
         // Create your app here
+        frame = new JFrame("Survery");
+        frame.setPreferredSize(new Dimension(200, 300));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        createComponents(frame.getContentPane());
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    private void createComponents(Container container) {
+        // Create the components for a survery UI
+        BoxLayout layout = new BoxLayout(container, BoxLayout.Y_AXIS);
+        container.setLayout(layout);
+        container.add(new JLabel("Are you?"));
+        container.add(new JCheckBox("Yes!"));
+        container.add(new JCheckBox("No!"));
+        
+        container.add(new JLabel("Why?"));
+        JRadioButton option1 = new JRadioButton("No reason.");
+        JRadioButton option2 = new JRadioButton("Because it is fun!");
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(option1);
+        buttonGroup.add(option2);
+        container.add(option1);
+        container.add(option2);
+        
+        container.add(new JButton("Done!"));
     }
 
 
